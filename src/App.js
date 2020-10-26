adc3c0a80bf55dfafd7ad2e8db87687fdb0f0aa1
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-10-26 16:52:32 +0100

    Added Bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return <div></div>;
+import Congrats from './components/Congrats';
+import GuessedWords from './components/GuessedWords';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords
+          guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
